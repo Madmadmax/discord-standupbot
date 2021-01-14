e2733ee3ab778fb3c153959941551ad36f8b4fb6
Maxim Chumbaev <maximchumbaev@gmail.com>
2021-01-14 11:15:53 +0300

    Reformat code, replace print for logging, add retries on send

--- standup/management/commands/run_bot.py
+++ standup/management/commands/run_bot.py
@@ -1,6 +1,7 @@
-import discord
 import asyncio
-from django.core.management.base import BaseCommand, CommandError
+import logging
+
+from django.core.management.base import BaseCommand
 from discord.ext.commands import Bot, MemberConverter, errors
 import discord
 from django.conf import settings
@@ -10,20 +11,23 @@ from standup import models
 import pytz
 
 
+logger = logging.getLogger(__name__)
+
+
 class Command(BaseCommand):
     help = 'Runs the Discord Bot'
 
     def handle(self, *args, **options):
         bot = Bot(command_prefix='!')
 
-        print('-----------------------------------')
-        print('Starting the bot...')
+        logger.info('-----------------------------------')
+        logger.info('Starting the bot...')
 
         @bot.event
         async def on_ready():
-            print('-----------------------------------')
-            print('Bot logged in as %s (%s)' % (bot.user.name, bot.user.id))
-            print('-----------------------------------')
+            logger.info('-----------------------------------')
+            logger.info(f'Bot logged in as {bot.user.name} ({bot.user.id})')
+            logger.info('-----------------------------------')
         
         bot.remove_command('help')
 
@@ -34,13 +38,40 @@ class Command(BaseCommand):
             except discord.errors.Forbidden:
                 pass
 
-            embed = discord.Embed(title="**StandupBot Help**", description="These commands are available:")
-            embed.add_field(name="**!timezones**", value="Shows all available timezones to pick from", inline=False)
-            embed.add_field(name="**!findtimezone <name>**", value="Shows all available timezones to pick from matching the given name, for easier lookup", inline=False)
-            embed.add_field(name="**!settimezone <tz_name>**", value="Set a timezone from the `!timezones` list", inline=False)
-            embed.add_field(name="**!mute_until <yyyy/mm/dd>**", value="Mute yourself from standup participation until a given date, good for vacations", inline=False)
-            embed.add_field(name="**!newstandup <standup_type>**", value="Start a new standup for the channel you are in", inline=False)
-            embed.add_field(name="**!addparticipant <standup_type> [readonly] <user 1> <user 2>...**", value="Add a new participant for a standup, optionally read only. You can add multiple add the same time", inline=False)
+            embed = discord.Embed(
+                title="**StandupBot Help**",
+                description="These commands are available:"
+            )
+            embed.add_field(
+                name="**!timezones**",
+                value="Shows all available timezones to pick from",
+                inline=False
+            )
+            embed.add_field(
+                name="**!findtimezone <name>**",
+                value="Shows all available timezones to pick from matching the given name, for easier lookup",  # noqa
+                inline=False
+            )
+            embed.add_field(
+                name="**!settimezone <tz_name>**",
+                value="Set a timezone from the `!timezones` list",
+                inline=False
+            )
+            embed.add_field(
+                name="**!mute_until <yyyy/mm/dd>**",
+                value="Mute yourself from standup participation until a given date, good for vacations",  # noqa
+                inline=False
+            )
+            embed.add_field(
+                name="**!newstandup <standup_type>**",
+                value="Start a new standup for the channel you are in",
+                inline=False
+            )
+            embed.add_field(
+                name="**!addparticipant <standup_type> [readonly] <user 1> <user 2>...**",  # noqa
+                value="Add a new participant for a standup, optionally read only. You can add multiple add the same time",  # noqa
+                inline=False
+            )
             
             await ctx.author.send(embed=embed)
         
@@ -50,23 +81,40 @@ class Command(BaseCommand):
             await ctx.message.delete()
 
             if not ctx.author.permissions_in(ctx.channel).manage_messages:
-                await ctx.author.send('Sorry, you have no permission to do this! Only users with the permission to manage messages for a given channel can do this.')
+                await ctx.author.send(
+                    'Sorry, you have no permission to do this! '
+                    'Only users with the permission to manage messages '
+                    'for a given channel can do this.'
+                )
                 return
 
             try:
-                stype = models.StandupType.objects.get(command_name=standup_type)
+                stype = models.StandupType.objects.get(
+                    command_name=standup_type
+                )
             except models.StandupType.DoesNotExist:
-                msg = 'Please provide a valid standup type as the argument of this function, your options are:\n\n'
-                msg += '\n'.join(['`%s` (%s)' % (s.command_name, s.name) for s in models.StandupType.objects.all()])
+                msg = (
+                    'Please provide a valid standup type as the argument '
+                    'of this function, your options are:\n\n'
+                )
+                msg += '\n'.join([
+                    f'`{s.command_name}` ({s.name})'
+                    for s in models.StandupType.objects.all()
+                ])
                 await ctx.author.send(msg)
                 return 
             
             today = timezone.now().date()
 
-            standup = models.Standup.objects.filter(event__standup_type=stype, event__channel__discord_channel_id=ctx.channel.id, event__standup_type__publish_to_channel=True, standup_date__lt=today).order_by('-id').first()
+            standup = models.Standup.objects.filter(
+                event__standup_type=stype,
+                event__channel__discord_channel_id=ctx.channel.id,
+                event__standup_type__publish_to_channel=True,
+                standup_date__lt=today
+            ).order_by('-id').first()
 
             if standup:
-                await ctx.author.send('Sending summary for %s' % standup)
+                await ctx.author.send(f'Sending summary for {standup}')
                 await standup.send_summary(bot)
             else:
                 await ctx.author.send('Standup not found, can\'t publish!')
@@ -84,14 +132,26 @@ class Command(BaseCommand):
                 read_only = True
             
             if not ctx.author.permissions_in(ctx.channel).manage_messages:
-                await ctx.author.send('Sorry, you have no permission to do this! Only users with the permission to manage roles for a given channel can do this.')
+                await ctx.author.send(
+                    'Sorry, you have no permission to do this! '
+                    'Only users with the permission to manage roles '
+                    'for a given channel can do this.'
+                )
                 return
 
             try:
-                stype = models.StandupType.objects.get(command_name=standup_type)
+                stype = models.StandupType.objects.get(
+                    command_name=standup_type
+                )
             except models.StandupType.DoesNotExist:
-                msg = 'Please provide a valid standup type as the argument of this function, your options are:\n\n'
-                msg += '\n'.join(['`%s` (%s)' % (s.command_name, s.name) for s in models.StandupType.objects.all()])
+                msg = (
+                    'Please provide a valid standup type as the argument '
+                    'of this function, your options are:\n\n'
+                )
+                msg += '\n'.join([
+                    f'`{s.command_name}` ({s.name})'
+                    for s in models.StandupType.objects.all()
+                ])
                 await ctx.author.send(msg)
                 return 
 
@@ -103,7 +163,11 @@ class Command(BaseCommand):
                 except errors.BadArgument:
                     continue
 
-                success, reason = models.StandupEvent.objects.add_participant_from_discord(stype, ctx.channel, mem, ctx.author, read_only)
+                success, reason = (
+                    models.StandupEvent.objects.add_participant_from_discord(
+                        stype, ctx.channel, mem, ctx.author, read_only
+                    )
+                )
 
                 if success:
                     members.append(mem)
@@ -111,18 +175,20 @@ class Command(BaseCommand):
                 if not success:
                     failed.append((mem, reason))
 
-
-                print(success, reason)
+                logger.info(f'{success}, {reason}')
             
             if members:
-                mstring = ', '.join(['<@%s>' % x.id for x in members])
-                await ctx.send('Added %s as participants of this standup!' % mstring)
+                mstring = ', '.join([f'<@{x.id}>' for x in members])
+                await ctx.send(
+                    f'Added {mstring} as participants of this standup!'
+                )
 
             if failed:
                 for mem, reason in failed:
-                    await ctx.send('Failed to add <@%s> as participants of this standup, %s.' % (mem.id, reason))
-
-
+                    await ctx.send(
+                        f'Failed to add <@{mem.id}> '
+                        f'as participants of this standup, {reason}.'
+                    )
 
         @bot.command(name='timezones')
         async def timezones(ctx):
@@ -132,14 +198,16 @@ class Command(BaseCommand):
             except discord.errors.Forbidden:
                 pass
 
-            await ctx.author.send('**You can choose from the following timezones:**')
+            await ctx.author.send(
+                '**You can choose from the following timezones:**'
+            )
             
-            # looping over slices of all timezones, working around max. message length of Discord
+            # looping over slices of all timezones, working around max.
+            # message length of Discord
             for i in range((len(pytz.common_timezones) // 75) + 1):
                 tzs = pytz.common_timezones[i*75:i*75+75]
-                msg = '`%s`' % '`, `'.join(tzs)
+                msg = f"`{'`, `'.join(tzs)}`"
                 await ctx.author.send(msg)
-        
 
         @bot.command(name='findtimezone')
         async def findtimezone(ctx, name):
@@ -148,15 +216,19 @@ class Command(BaseCommand):
             except discord.errors.Forbidden:
                 pass
 
-            await ctx.author.send('**Found the following timezones for `%s`:**' % name)
+            await ctx.author.send(
+                f'**Found the following timezones for `{name}`:**'
+            )
             
-            # looping over slices of all timezones, working around max. message length of Discord
-            ftzs = [x for x in pytz.common_timezones if name.lower() in x.lower()]
+            # looping over slices of all timezones, working around max.
+            # message length of Discord
+            ftzs = [
+                x for x in pytz.common_timezones if name.lower() in x.lower()
+            ]
             for i in range((len(ftzs) // 75) + 1):
                 tzs = ftzs[i*75:i*75+75]
-                msg = '`%s`' % '`, `'.join(tzs)
+                msg = f"`{'`, `'.join(tzs)}`"
                 await ctx.author.send(msg)
-        
 
         @bot.command(name='settimezone')
         async def settimezone(ctx, timezonename):
@@ -177,9 +249,15 @@ class Command(BaseCommand):
                 user.timezone = timezonename
                 user.save()
 
-                await ctx.author.send('Thanks, your timezone has been set to %s' % user.timezone)
+                await ctx.author.send(
+                    f'Thanks, your timezone has been set to {user.timezone}'
+                )
             else:
-                await ctx.author.send('%s is a unknown timezone, please execute the `!timezones` command to see all avaiable timezones!' % timezonename)
+                await ctx.author.send(
+                    f'{timezonename} is a unknown timezone, '
+                    f'please execute the `!timezones` command '
+                    f'to see all avaiable timezones!'
+                )
 
         @bot.command(name='mute_until')
         async def mute_until(ctx, date):
@@ -190,8 +268,13 @@ class Command(BaseCommand):
         
             try:
                 until = datetime.date(*[int(x) for x in date.split('/')])
-            except:
-                await ctx.author.send('Unable to mute you, date format unknown. Please provide a date like this: YYYY/MM/DD, so for example `!mute_until 2020/01/01`')
+            except Exception as e:
+                logger.warning(str(e))
+                await ctx.author.send(
+                    'Unable to mute you, date format unknown. '
+                    'Please provide a date like this: YYYY/MM/DD, '
+                    'so for example `!mute_until 2020/01/01`'
+                )
                 
             user, _ = models.User.objects.get_or_create(
                 discord_id=ctx.author.id, 
@@ -204,33 +287,53 @@ class Command(BaseCommand):
             user.mute_until = until
             user.save()
 
-            await ctx.author.send('Thanks, you won\'t participate in standups until %s' % until)
-
+            await ctx.author.send(
+                f"Thanks, you won't participate in standups until {until}"
+            )
 
         @bot.command(name='newstandup')
         async def newstandup(ctx, standup_type):
             
             try:
-                stype = models.StandupType.objects.get(command_name=standup_type)
+                stype = models.StandupType.objects.get(
+                    command_name=standup_type
+                )
             except models.StandupType.DoesNotExist:
-                msg = 'Please provide a valid standup type as the argument of this function, your options are:\n\n'
-                msg += '\n'.join(['`%s` (%s)' % (s.command_name, s.name) for s in models.StandupType.objects.all()])
+                msg = (
+                    'Please provide a valid standup type as the argument '
+                    'of this function, your options are:\n\n'
+                )
+                msg += '\n'.join([
+                    f'`{s.command_name}` ({s.name})'
+                    for s in models.StandupType.objects.all()
+                ])
                 await ctx.author.send(msg)
                 await ctx.message.delete()
                 return 
 
             if not ctx.author.permissions_in(ctx.channel).manage_messages:
-                await ctx.author.send('Sorry, you have no permission to do this! Only users with the permission to manage roles for a given channel can do this.')
+                await ctx.author.send(
+                    'Sorry, you have no permission to do this! '
+                    'Only users with the permission to manage roles '
+                    'for a given channel can do this.'
+                )
             else:
-                if models.StandupEvent.objects.create_from_discord(stype, ctx.channel, ctx.author):
-                    await ctx.send('%s initialized for this channel!' % stype.name)
+                if (
+                    models.StandupEvent.objects.create_from_discord(
+                        stype, ctx.channel, ctx.author
+                    )
+                ):
+                    await ctx.send(
+                        f'{stype.name} initialized for this channel!'
+                    )
                 else:
-                    await ctx.send('This channel already has a %s, no new one was created.' % stype.name)
-
+                    await ctx.send(
+                        f'This channel already has a {stype.name}, '
+                        f'no new one was created.'
+                    )
 
             await ctx.message.delete()
 
-	    
         async def interval():
             await asyncio.sleep(10)
 
@@ -240,31 +343,50 @@ class Command(BaseCommand):
                 # Repetitive task checks here
                 for ev in models.StandupEvent.objects.all():
                     success, to_notify = ev.initiate()
-                    if success:
-                        for participant in to_notify:
-                            did = int(participant.user.discord_id)
-                            user = bot.get_user(did)
+                    if not success:
+                        continue
+                    for participant in to_notify:
+                        did = int(participant.user.discord_id)
+                        user = bot.get_user(did)
+                        for i in range(5):
                             try:
 
                                 if not participant.read_only:
-                                    await user.send('Please answer the questions for "%s" in "%s" here: %s - Thanks!' % (
-                                        participant.standup.event.standup_type.name, 
-                                        participant.standup.event.channel, 
-                                        participant.get_form_url(),))
+                                    await user.send(
+                                        f'Please answer the questions '
+                                        f'for "{participant.standup.event.standup_type.name}" '  # noqa
+                                        f'in "{participant.standup.event.channel}" '  # noqa
+                                        f'here: {participant.get_form_url()} - Thanks!'  # noqa
+                                    )
 
-                                await user.send('You can view your private standup overview here: %s' % (
-                                participant.get_home_url(),))
+                                await user.send(
+                                    f'You can view your '
+                                    f'private standup overview '
+                                    f'here: {participant.get_home_url()}'
+                                )
+                                break
                             except Exception as e:
-                                print('Something went wrong while sending form to the user: %s' % e)
+                                logger.error(
+                                    f'Something went wrong while sending form '
+                                    f'to the user: {e}'
+                                )
+                                logger.info(f'Trying to resend ({i})...')
+                                await asyncio.sleep(2)
 
-                for standup in models.Standup.objects.filter(pinned_message_id__isnull=True, rebuild_message=True, event__standup_type__publish_to_channel=True):
+                standups = models.Standup.objects.filter(
+                    pinned_message_id__isnull=True,
+                    rebuild_message=True,
+                    event__standup_type__publish_to_channel=True
+                )
+                for standup in standups:
                     await standup.send_summary(bot)
-                                
 
                 await asyncio.sleep(10)
 
         try:
-            bot.loop.run_until_complete(asyncio.gather(bot.start(settings.DISCORD_TOKEN), interval()))
+            bot.loop.run_until_complete(
+                asyncio.gather(bot.start(settings.DISCORD_TOKEN), interval())
+            )
         except KeyboardInterrupt:
             bot.loop.run_until_complete(bot.logout())
         finally:
