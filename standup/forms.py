5b6010f780577e961988f93c125963089e893586
Maxim Chumbaev <maximchumbaev@gmail.com>
2021-02-10 10:13:06 +0300

    Revert "Reformat code, fix help text for optional field"

--- standup/forms.py
+++ standup/forms.py
@@ -20,48 +20,23 @@ class StandupForm(forms.Form):
         answers = {}
 
         if self.participation.completed:
-            answers = dict([
-                (a.question, a.answer) for a in self.participation.answers.all()
-            ])
+            answers = dict([(a.question, a.answer) for a in self.participation.answers.all()])
 
-        questions = participation.standup.event.standup_type.questions.all()
-        for question in questions:
+        for question in participation.standup.event.standup_type.questions.all():
             field_name = 'question_%d' % question.id
-            self.fields[field_name] = forms.CharField(
-                required=False,
-                label=question.question,
-                initial=answers.get(question),
-                widget=forms.Textarea()
-            )
-            if not question.important:
-                self.fields[field_name].help_text = (
-                    'This field is optional, '
-                    'please leave it empty if it does not apply!'
-                )
+            self.fields[field_name] = forms.CharField(required=False, label=question.question, initial=answers.get(question), widget=forms.Textarea())
+            if question.important:
+                self.fields[field_name].help_text = 'This field is optional, please leave it empty if it does not apply!'
 
-            if (
-                question.prefill_last_answer
-                and prev_parti
-                and not self.participation.completed
-            ):
-                lastans = (
-                    prev_parti.answers
-                    .filter(question=question.prefill_last_answer)
-                    .first()
-                )
+            if question.prefill_last_answer and prev_parti and not self.participation.completed:
+                lastans = prev_parti.answers.filter(question=question.prefill_last_answer).first()
                 if lastans:
                     self.fields[field_name].initial = lastans.answer  
 
     def save(self):
-        questions = (
-            self.participation.standup.event.standup_type.questions.all()
-        )
-        for question in questions:
+        for question in self.participation.standup.event.standup_type.questions.all():
             field_name = 'question_%d' % question.id
-            ans, created = self.participation.answers.get_or_create(
-                question=question,
-                defaults={'answer': self.cleaned_data[field_name]}
-            )
+            ans, created = self.participation.answers.get_or_create(question=question, defaults={'answer': self.cleaned_data[field_name]})
             if not created:
                 ans.answer = self.cleaned_data[field_name]
                 ans.save()
